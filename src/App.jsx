7e1a25919b3df1a6bc198efcbbc19726f706e0d9
biswasbittu <biswasbittu7415@yahoo.com>
2024-03-06 17:34:30 +0530

    ready for start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold text-center bg-slate-500'>Next-Page</h1>
+    </div>
+  );
+};
+
+export default App;
